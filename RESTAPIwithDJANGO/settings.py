2d3bd9defed0a1f1056bcf2d3ded08a76d7e01c4
juanVelez <mrjiuby@gmail.com>
2023-12-26 01:32:29 -0500

    mas cosas para el deploy

--- RESTAPIwithDJANGO/settings.py
+++ RESTAPIwithDJANGO/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'RESTAPIwithDJANGO.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
 }
